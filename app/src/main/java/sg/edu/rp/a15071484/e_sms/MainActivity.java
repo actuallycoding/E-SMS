26c3c4de342490eed0d9e0b38e233f3cb4542f86
actuallycoding <15017484@myrp.edu.sg>
2017-08-02 16:37:53 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/a15071484/e_sms/MainActivity.java
@@ -0,0 +1,13 @@
+package sg.edu.rp.a15071484.e_sms;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
